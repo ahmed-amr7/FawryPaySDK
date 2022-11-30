4156c53d81cc953d5f56153fd11a2bd4b546be34
Ahmed Amr <ahmed.amr@fawry.com>
2022-11-30 20:13:08 +0200

    add xcframework

---
+++ FawryPaySDK.xcframework/ios-arm64/FawryPaySDK.framework/Headers/FawryPaySDK.h
@@ -0,0 +1,18 @@
+//
+//  FawryPaySDK.h
+//  FawryPaySDK
+//
+//  Created by Ahmed Sharf on 10/11/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FawryPaySDK.
+FOUNDATION_EXPORT double FawryPaySDKVersionNumber;
+
+//! Project version string for FawryPaySDK.
+FOUNDATION_EXPORT const unsigned char FawryPaySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FawryPaySDK/PublicHeader.h>
+
+
